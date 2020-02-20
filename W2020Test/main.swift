aa7519c889d6b7e57152c6207d1f1be850c9610b
roop <roop>
2020-02-19 19:00:10 -0500

    [roop] json

--- W2020Test/main.swift
+++ W2020Test/main.swift
@@ -9,12 +9,12 @@
 import Foundation
 func readTextFile(FileName : String)
 {
-    print(Bundle.main.bundlePath)
+   // print(Bundle.main.bundlePath)
     if let path = Bundle.main.path(forResource: FileName, ofType: "txt")
     {
         
     
-    print(path)
+  //  print(path)
         let str = try? String(contentsOfFile: path)
         if let s = str
         {
@@ -28,18 +28,54 @@ func readTextFile(FileName : String)
     }
     
 }
-readTextFile(FileName: "Data")
+//readTextFile(FileName: "Data")
 
-func readJSONUserInfo(FileName : String)
+//func readJSONUserInfo(FileName : String)
+//{
+   // if let path1 = Bundle.main.path(forResource: FileName, ofType: "json")
+  //  {
+   // print(path1)
+    
+    //let str = try? String(contentsOfFile: path1)
+    //if let s = str
+    //{
+       // print(s)
+ 
+   // else
+   // {
+     //   print("error while reading file")
+    //}
+    
+   // }
+    
+    
+
+
+//readJSONUserInfo(FileName : "SingleUser")
+func readJSONUserInfo(fileName : String)
 {
-    if let path = Bundle.main.path(forResource: FileName, ofType: "json")
+    let filePath = Bundle.main.url(forResource: fileName, withExtension : "json")
+    
+        guard let path = filePath else {
+            print("invalid file path")
+            return
+        }
+        guard let data = try? Data(contentsOf: path) else {
+            print("error while read data from url")
+            return
+        }
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+            print("error while reading json data")
+            return
+        }
+        print(json)
+    if let userDictionary = json as? [String : Any]
     {
-    print(path)
+        print(userDictionary)
     }
     
-    
 }
-
+readJSONUserInfo(fileName: "SingleUser")
 
 
 
